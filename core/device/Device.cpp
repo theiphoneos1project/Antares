d69aa3abcc954b86eafde20991f22adee31752cd
NightwindDev <81449663+NightwindDev@users.noreply.github.com>
2026-09-09 16:58:56 -0400

    Add copyright headers

--- core/device/Device.cpp
+++ core/device/Device.cpp
@@ -1,3 +1,7 @@
+//
+// Copyright (c) 2026 Nightwind
+//
+
 #include "Device.hpp"
 #include <iostream>
 #include <cstring>
--- core/device/Device.hpp
+++ core/device/Device.hpp
@@ -1,3 +1,7 @@
+//
+// Copyright (c) 2026 Nightwind
+//
+
 #ifndef DEVICE_H
 #define DEVICE_H
 
